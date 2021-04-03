5e44de4c681b0e23dd2f43d91f63be82de2e74b3
Code Sandbox <hello@codesandbox.io>
2021-04-03 14:18:31 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.889476f3f71b1195c418c7af8b6b59d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99aa5e993eb96c2692819d49d1ef4baa",
+    "url": "https://liamcho.github.io/csb-txwpe/index.html"
+  },
+  {
+    "revision": "b31710e3df3141d2f2f3",
+    "url": "https://liamcho.github.io/csb-txwpe/static/css/2.75eab170.chunk.css"
+  },
+  {
+    "revision": "8ba25a0173c2a735a1f8",
+    "url": "https://liamcho.github.io/csb-txwpe/static/css/main.12903076.chunk.css"
+  },
+  {
+    "revision": "b31710e3df3141d2f2f3",
+    "url": "https://liamcho.github.io/csb-txwpe/static/js/2.804f297c.chunk.js"
+  },
+  {
+    "revision": "8ba25a0173c2a735a1f8",
+    "url": "https://liamcho.github.io/csb-txwpe/static/js/main.8e6515a7.chunk.js"
+  },
+  {
+    "revision": "ff3c72063296ac480c66",
+    "url": "https://liamcho.github.io/csb-txwpe/static/js/runtime~main.58f1b285.js"
+  }
+]);
